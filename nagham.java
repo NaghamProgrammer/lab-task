fd98684a4f225458bc4eecc4849a17a4c0bb65c3
Nagham Wael <161651186+NaghamProgrammer@users.noreply.github.com>
2025-05-07 19:06:05 +0300

    20230054
    20230054

--- nagham.java
+++ nagham.java
@@ -1,6 +1,6 @@
 public class PrintInfo {
     public static void main(String[] args) {
-        System.out.println("Nagham Wael");
-        System.out.println("20231189");
+        System.out.println("Nagham");
+        
     }
 }
